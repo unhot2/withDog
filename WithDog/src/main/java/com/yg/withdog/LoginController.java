14a03b6b8cef8dca2a847098425a767c9b690988
unhot2 <62008631+unhot2@users.noreply.github.com>
2020-12-02 22:23:46 +0900

    기본설정, header 드롭다운메뉴 작성

---
+++ WithDog/src/main/java/com/yg/withdog/LoginController.java
@@ -0,0 +1,13 @@
+package com.yg.withdog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("login")
+	public String loginForm() {
+		return "/login/loginForm";
+	}
+}
